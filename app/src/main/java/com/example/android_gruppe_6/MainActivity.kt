8bff1927c21b54a1e31f92cb81b8cdc660e5b139
hgr069 <hgr069@post.uit.no>
2022-05-19 17:46:21 +0200

    Satte opp navigation.

--- app/src/main/java/com/example/android_gruppe_6/MainActivity.kt
+++ app/src/main/java/com/example/android_gruppe_6/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.android_gruppe_6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import com.example.android_gruppe_6.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -9,10 +13,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
+  /*      if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
-        }
+        }*/
+        val myToolbar: Toolbar = findViewById(R.id.my_toolbar)
+        myToolbar.setTitle(R.string.app_name)
+        setSupportActionBar(myToolbar)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration.Builder(navController.graph)
+            .build()
+        NavigationUI.setupWithNavController(myToolbar, navController, appBarConfiguration)
+
     }
 }
