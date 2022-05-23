7e50e94ec4036e0490328819d7566bf8ceaa66e2
hgr069 <hgr069@post.uit.no>
2022-05-23 23:58:23 +0200

    Satte opp google maps

---
+++ app/src/main/java/com/example/android_gruppe_6/MapsFragment.kt
@@ -0,0 +1,95 @@
+package com.example.android_gruppe_6
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import androidx.fragment.app.Fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.*
+
+import com.google.android.gms.maps.model.LatLng
+
+
+class MapsFragment : Fragment() {
+    private val REQUEST_LOCATION_PERMISSION = 1
+    private lateinit var map: GoogleMap
+
+    @SuppressLint("MissingPermission")
+    private val callback = OnMapReadyCallback { googleMap ->
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * In this case, we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to
+         * install it inside the SupportMapFragment. This method will only be triggered once the
+         * user has installed Google Play services and returned to the app.
+         */
+        val stroemsund = LatLng(63.9, 15.6)
+        val zoomlevel = 4f
+        /*googleMap.addMarker(MarkerOptions().position(stroemsund).title("Marker in Stroemsund"))*/
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(stroemsund, zoomlevel))
+        activity?.let { MapsInitializer.initialize(it) }
+        map = googleMap
+        map.setMyLocationEnabled(true)
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_maps, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+
+    }
+
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray) {
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults.contains(PackageManager.PERMISSION_GRANTED)) {
+                enableMyLocation()
+            }
+        }
+    }
+
+    private fun isPermissionGranted() : Boolean {
+        return activity?.let {
+            ContextCompat.checkSelfPermission(
+                it,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+        } == PackageManager.PERMISSION_GRANTED
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun enableMyLocation() {
+        if (isPermissionGranted()) {
+            map.isMyLocationEnabled = true
+        }
+        else {
+            activity?.let {
+                ActivityCompat.requestPermissions(
+                    it,
+                    arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION),
+                    REQUEST_LOCATION_PERMISSION
+                )
+            }
+        }
+    }
+}
