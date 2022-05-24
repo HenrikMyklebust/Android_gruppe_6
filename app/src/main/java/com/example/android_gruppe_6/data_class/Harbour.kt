2eec7e641857807956b9762419056b8d1a2a896d
Magnus Ree <mre056@post.uit.no>
2022-05-24 17:13:52 +0200

    har lagt til ShowTideFragment og en Harbour dataclass for havner

---
+++ app/src/main/java/com/example/android_gruppe_6/data_class/Harbour.kt
@@ -0,0 +1,11 @@
+package com.example.android_gruppe_6.data_class
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Harbour(
+    val name: String,
+    val lat: Double,
+    val long: Double
+): Parcelable
