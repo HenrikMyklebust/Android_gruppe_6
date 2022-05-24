920d71c683f53484f6981797b959a1504d8dc4bc
Magnus Ree <mre056@post.uit.no>
2022-05-24 15:35:41 +0200

    Har lagt til logo

---
+++ app/src/main/java/com/example/android_gruppe_6/data_class/Tide.kt
@@ -0,0 +1,17 @@
+package com.example.android_gruppe_6.data_class
+
+data class Tide(
+    val year: Int,
+    val month: Int,
+    val day: Int,
+    val hour: Int,
+    val progNr: Int,
+    val surge: Double,
+    val tide: Double,
+    val total: Double,
+    val prob0: Double,
+    val prob25: Double,
+    val prob50: Double,
+    val prob75: Double,
+    val prob100: Double
+)
