bc761009b0c2b5cfff3955c5022399ae35c5bd08
hgr069 <hgr069@post.uit.no>
2022-05-25 23:46:48 +0200

    Byttet markør til ikon med logo

--- app/src/main/java/com/example/android_gruppe_6/MapsFragment.kt
+++ app/src/main/java/com/example/android_gruppe_6/MapsFragment.kt
@@ -2,7 +2,10 @@ package com.example.android_gruppe_6
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.location.Location
 import androidx.fragment.app.Fragment
 
@@ -17,6 +20,8 @@ import com.example.android_gruppe_6.domain.getHarbors
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
@@ -50,7 +55,8 @@ class MapsFragment : Fragment() {
         val harbors = getHarbors()
         for (harbor in harbors) {
             var coordinates = LatLng(harbor.lat, harbor.lon)
-            googleMap.addMarker(MarkerOptions().position(coordinates).title("Harbour"))
+            googleMap.addMarker(MarkerOptions().position(coordinates).title("Harbour").icon(
+                activity?.let { bitmapDescriptorFromVector(it, R.drawable.ic_icon_grey_small) }))
 
         }
 
@@ -155,6 +161,15 @@ class MapsFragment : Fragment() {
             }
         } catch (e: SecurityException) {
             Log.e("Exception: %s", e.message, e)
+        }
+    }
+
+    private fun bitmapDescriptorFromVector(context: Context, vectorResId: Int): BitmapDescriptor? {
+        return ContextCompat.getDrawable(context, vectorResId)?.run {
+            setBounds(0, 0, intrinsicWidth, intrinsicHeight)
+            val bitmap = Bitmap.createBitmap(intrinsicWidth, intrinsicHeight, Bitmap.Config.ARGB_8888)
+            draw(Canvas(bitmap))
+            BitmapDescriptorFactory.fromBitmap(bitmap)
         }
     }
 
