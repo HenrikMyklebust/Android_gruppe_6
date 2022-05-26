485e58eca9989d0ea887db84d963e397c60b5951
Henrik Myklebust <hmy026@post.uit.no>
2022-05-26 14:19:08 +0200

    Lagt til alternativer ved uthenting av info fra databasen

--- app/src/main/java/com/example/android_gruppe_6/repository/TideRepository.kt
+++ app/src/main/java/com/example/android_gruppe_6/repository/TideRepository.kt
@@ -7,6 +7,8 @@ import com.example.android_gruppe_6.network.TideNetworkGet
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.lang.Exception
+import java.time.LocalDateTime
+import java.time.temporal.ChronoField
 import java.util.stream.IntStream.range
 
 class TideRepository(private val database: HarborsDatabase) {
@@ -50,12 +52,87 @@ class TideRepository(private val database: HarborsDatabase) {
     }
 
     // Get data from specific harbor
-    suspend fun getDataDB(harborName: String): List<HarborWithTide> {
+    suspend fun getDbHarborWithTide(harborName: String): List<HarborWithTide> {
         var response: List<HarborWithTide>
         withContext(Dispatchers.IO){
             response = database.harborDao.getHarborWithTide(harborName)
         }
         return response
+    }
+
+    // Get tide from harbor
+    suspend fun getDbTide(harborName: String): List<TideData> {
+        var response: List<DbTide>
+        withContext(Dispatchers.IO){
+            response = database.harborDao.getTide(harborName)
+        }
+        return response.map { TideData(
+            harbor = it.harbor,
+            year = it.year,
+            month = it.month,
+            day = it.day,
+            hour = it.hour,
+            prognosis_len = it.prognosis_len,
+            surge = it.surge,
+            tide = it.tide,
+            total = it.total,
+            p0 = it.p0,
+            p25 = it.p25,
+            p50 = it.p50,
+            p75 = it.p75,
+            p100 = it.p100
+        ) }
+    }
+
+    // Get tide one day and harbor
+    suspend fun getDbTideOneDay(harborName: String, day: Int): List<TideData> {
+        var response: List<DbTide>
+        withContext(Dispatchers.IO){
+            response = database.harborDao.getTideOneDay(harborName, day)
+        }
+        return response.map { TideData(
+            harbor = it.harbor,
+            year = it.year,
+            month = it.month,
+            day = it.day,
+            hour = it.hour,
+            prognosis_len = it.prognosis_len,
+            surge = it.surge,
+            tide = it.tide,
+            total = it.total,
+            p0 = it.p0,
+            p25 = it.p25,
+            p50 = it.p50,
+            p75 = it.p75,
+            p100 = it.p100
+        ) }
+    }
+
+    // Get tide from harbor today
+    suspend fun getDbTideToday(harborName: String): List<TideData>{
+        var response: List<DbTide>
+
+        withContext(Dispatchers.IO){
+            response = database.harborDao.getTideOneDay(
+                harborName,
+                LocalDateTime.now().get(ChronoField.DAY_OF_MONTH))
+        }
+        return response.map { TideData(
+            harbor = it.harbor,
+            year = it.year,
+            month = it.month,
+            day = it.day,
+            hour = it.hour,
+            prognosis_len = it.prognosis_len,
+            surge = it.surge,
+            tide = it.tide,
+            total = it.total,
+            p0 = it.p0,
+            p25 = it.p25,
+            p50 = it.p50,
+            p75 = it.p75,
+            p100 = it.p100
+        ) }
     }
 
     // Insert data on tide
