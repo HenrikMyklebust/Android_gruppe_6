f74153b7aa3d286a372822665ebe4b736a8c2855
Magnus Ree <mre056@post.uit.no>
2022-05-26 12:22:20 +0200

    Bottom navigation virker. Men login fragmentet krasjer siden

--- app/src/main/java/com/example/android_gruppe_6/MainActivity.kt
+++ app/src/main/java/com/example/android_gruppe_6/MainActivity.kt
@@ -3,15 +3,22 @@ package com.example.android_gruppe_6
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.example.android_gruppe_6.database.getDatabase
+import com.example.android_gruppe_6.harborlist.HarborlistFragment
 import com.example.android_gruppe_6.repository.TideRepository
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private val harborListFragment = HarborlistFragment()
+    private val mapsFragment = MapsFragment()
+    private val loginFragment = LoginFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +39,26 @@ class MainActivity : AppCompatActivity() {
         NavigationUI.setupWithNavController(myToolbar, navController, appBarConfiguration)
 
         val repository = TideRepository(getDatabase(application))
+
+        replaceFragment(harborListFragment)
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
+        bottomNav.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.bnbListView -> replaceFragment(harborListFragment)
+                R.id.bnbMapView -> replaceFragment(mapsFragment)
+                R.id.bnbLogIn -> replaceFragment(loginFragment)
+                else -> false
+            }
+            true
+        }
+
+    }
+    private fun replaceFragment(fragment: Fragment){
+        if (fragment != null){
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.nav_host_fragment, fragment)
+            transaction.commit()
+        }
 
     }
 }
